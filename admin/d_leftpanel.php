23dcde064f175c0d40f9956e8758410aa18974b8
YeaminRabbi <yeaminrabbi308@gmail.com>
2021-11-14 00:53:06 +0600

    initial project

---
+++ admin/d_leftpanel.php
@@ -0,0 +1,102 @@
+<!-- ########## START: LEFT PANEL ########## -->
+    <div class="sl-logo" style="font-size: 19px;"><a href="index.php"><i class="icon ion-android-star-outline"></i> Official Inventory</a></div>
+    
+    <div class="sl-sideleft">
+      
+      <label class="sidebar-label">Navigation</label>
+      <div class="sl-sideleft-menu">
+        <a href="index.php" class="sl-menu-link">
+          <div class="sl-menu-item">
+            <i class="menu-item-icon icon ion-ios-home-outline tx-22"></i>
+            <span class="menu-item-label">Dashboard</span>
+          </div><!-- menu-item -->
+        </a><!-- sl-menu-link -->
+        
+        <a href="#" class="sl-menu-link">
+          <div class="sl-menu-item">
+            <i class="fa fa-product-hunt" aria-hidden="true"></i>
+            <span class="menu-item-label">Products</span>
+            <i class="menu-item-arrow fa fa-angle-down"></i>
+
+          </div><!-- menu-item -->
+        </a><!-- sl-menu-link -->
+        <ul class="sl-menu-sub nav flex-column">
+          <li class="nav-item"><a href="product_create.php" class="nav-link">Create/Insert</a></li>
+          <li class="nav-item"><a href="product_list.php" class="nav-link">List</a></li>
+        </ul>
+
+        
+
+        <a href="#" class="sl-menu-link">
+          <div class="sl-menu-item">
+            <i class="menu-item-icon ion-ios-pie-outline tx-20"></i>
+            <span class="menu-item-label">Category</span>
+            <i class="menu-item-arrow fa fa-angle-down"></i>
+          </div><!-- menu-item -->
+        </a><!-- sl-menu-link -->
+        <ul class="sl-menu-sub nav flex-column">
+          <li class="nav-item"><a href="category_create.php" class="nav-link">Create/Insert</a></li>
+          <li class="nav-item"><a href="category_list.php" class="nav-link">List</a></li>
+        </ul>
+
+
+
+        <a href="#" class="sl-menu-link">
+          <div class="sl-menu-item">
+            <i class="fa fa-user" aria-hidden="true"></i>
+            <span class="menu-item-label">Users</span>
+            <i class="menu-item-arrow fa fa-angle-down"></i>
+          </div><!-- menu-item -->
+        </a><!-- sl-menu-link -->
+        <ul class="sl-menu-sub nav flex-column">
+          <li class="nav-item"><a href="user_account_create.php" class="nav-link">Add User</a></li>
+          <li class="nav-item"><a href="user_account_approval_list.php" class="nav-link">Account Approval</a></li>
+          <li class="nav-item"><a href="user_account_list.php" class="nav-link">User List</a></li>
+        </ul>
+
+
+        <a href="#" class="sl-menu-link">
+          <div class="sl-menu-item">
+           <i class="fa fa-graduation-cap" aria-hidden="true"></i>
+            <span class="menu-item-label">Schools</span>
+            <i class="menu-item-arrow fa fa-angle-down"></i>
+          </div><!-- menu-item -->
+        </a><!-- sl-menu-link -->
+        <ul class="sl-menu-sub nav flex-column">
+          <li class="nav-item"><a href="school_insert.php" class="nav-link">Create/Insert</a></li>
+          <li class="nav-item"><a href="school_list.php" class="nav-link">List</a></li>
+        </ul>
+
+
+        <a href="#" class="sl-menu-link">
+          <div class="sl-menu-item">
+           <i class="fa fa-dot-circle-o" aria-hidden="true"></i>
+            <span class="menu-item-label">Division</span>
+            <i class="menu-item-arrow fa fa-angle-down"></i>
+          </div><!-- menu-item -->
+        </a><!-- sl-menu-link -->
+        <ul class="sl-menu-sub nav flex-column">
+          <li class="nav-item"><a href="division_insert.php" class="nav-link">Create/Insert</a></li>
+          <li class="nav-item"><a href="division_list.php" class="nav-link">List</a></li>
+        </ul>
+
+
+
+        <a href="#" class="sl-menu-link">
+          <div class="sl-menu-item">
+           <i class="fa fa-first-order" aria-hidden="true"></i>
+            <span class="menu-item-label">Requisition</span>
+            <i class="menu-item-arrow fa fa-angle-down"></i>
+          </div><!-- menu-item -->
+        </a><!-- sl-menu-link -->
+        <ul class="sl-menu-sub nav flex-column">
+          <li class="nav-item"><a href="order_pending.php" class="nav-link">Pending</a></li>
+          <li class="nav-item"><a href="order_confirmed.php" class="nav-link">Delivered</a></li>
+        </ul>
+
+
+      </div><!-- sl-sideleft-menu -->
+
+      <br>
+    </div><!-- sl-sideleft -->
+    <!-- ########## END: LEFT PANEL ########## -->
---
+++ user/d_leftpanel.php
@@ -0,0 +1,47 @@
+<!-- ########## START: LEFT PANEL ########## -->
+    <div class="sl-logo" style="font-size: 19px;"><a href="index.php"><i class="icon ion-android-star-outline"></i> PMCMS</a></div>
+    
+    <div class="sl-sideleft">
+      
+      <label class="sidebar-label">Criterias</label>
+      <div class="sl-sideleft-menu">
+
+         <a href="product.php?product_master_category=ELECTRONICS" class="sl-menu-link">
+          <div class="sl-menu-item">
+            <i class="menu-item-icon ion-ios-pie-outline tx-20"></i>
+            <span class="menu-item-label">Home</span>
+          </div><!-- menu-item -->
+        </a><!-- sl-menu-link -->
+
+         <a href="product.php?product_master_category=SOFTWARE" class="sl-menu-link">
+          <div class="sl-menu-item">
+            <i class="menu-item-icon ion-ios-pie-outline tx-20"></i>
+            <span class="menu-item-label">Courses</span>
+          </div><!-- menu-item -->
+        </a><!-- sl-menu-link -->
+
+        <a href="product.php?product_master_category=HARDWARE" class="sl-menu-link">
+          <div class="sl-menu-item">
+            <i class="menu-item-icon ion-ios-pie-outline tx-20"></i>
+            <span class="menu-item-label">Predictive Function</span>
+          </div><!-- menu-item -->
+
+        </a><!-- sl-menu-link -->
+
+      
+        <label class="sidebar-label">Session</label>
+
+
+        <a href="requisition_history.php" class="sl-menu-link">
+          <div class="sl-menu-item">
+            <i class="fa fa-bars" aria-hidden="true"></i>
+            <span class="menu-item-label">Logout</span>
+          </div><!-- menu-item -->
+        </a><!-- sl-menu-link -->
+
+       
+      </div><!-- sl-sideleft-menu -->
+
+      <br>
+    </div><!-- sl-sideleft -->
+    <!-- ########## END: LEFT PANEL ########## -->
